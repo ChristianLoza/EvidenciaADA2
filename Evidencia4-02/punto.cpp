f91fd627934baab07c1bd43a90b10dda698d6c0d
Christian Loza <Christianlzap@gmail.com>
2015-11-22 16:25:41 -0500

    No changes...

---
+++ Evidencia4-02/punto.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "punto.h"
+
+
+punto::punto(void)
+{
+}
+
+
+punto::~punto(void)
+{
+}
---
+++ Evidencia4-02/punto.h
@@ -0,0 +1,10 @@
+#pragma once
+
+const double valCero= 1e-10;
+class punto
+{
+public:
+	punto(void);
+	~punto(void);
+};
+
